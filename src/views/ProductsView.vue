df52a9e6feef02031523bcbf14fe5f60d1639b11
stevecyj <ycsteven.tsao@gmail.com>
2022-02-28 05:40:34 +0800

    [ProductsView]:cards

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -3,14 +3,14 @@
   <!-- card -->
   <div class="container">
     <!-- row 決定內層的數量 -->
-    <div class="row row-cols-4">
+    <div class="row row-cols-1 row-cols-lg-4 g-3">
       <!-- 內層不定義寬度 -->
       <div v-for="product in products" :key="product.id" class="col">
-        <div class="card" style="">
-          <img src="" class="card-img-top" alt="..." />
+        <div class="card h-100" style="">
+          <img :src="product.imageUrl" class="card-img-top" alt="..." />
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example</p>
+            <h5 class="card-title">{{ product.title }}</h5>
+            <p class="card-text">{{ product.description }}</p>
             <a href="#" class="btn btn-primary">Go somewhere</a>
           </div>
         </div>
