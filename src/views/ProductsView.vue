1df68c7d845f28e05693d22b3804a2a5febb6aa0
stevecyj <ycsteven.tsao@gmail.com>
2022-02-27 17:41:49 +0800

    [Products]:use vue-axios

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,3 +1,29 @@
 <template>
   <h2>產品列表</h2>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [],
+      product: {
+        name: '',
+        price: 0,
+        description: '',
+      },
+    };
+  },
+  methods: {
+    getProducts() {
+      this.$http.get(`${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/products/all`).then((res) => {
+        // this.products = res.data;
+        console.log(res);
+      });
+    },
+  },
+  mounted() {
+    this.getProducts();
+  },
+};
+</script>
